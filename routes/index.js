a31ae2964288a5ee89d11f19e16a53de20a2bb50
andif_injani@bitbucket.com <andif@injani.com>
2019-03-04 15:28:29 +0700

    ok

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-
+var imagePath = appRoot+"/public/images";
 /* GET home page. */
 router.get('/', async function(req, res, next) {
 
   const PiCamera = require('pi-camera');
   const myCamera = new PiCamera({
     mode: 'photo',
-    output: appRoot+'/tost.jpg',
+    output: imagePath+'/tost.jpg',
     width: 640,
     height: 480,
     nopreview: true,
@@ -17,7 +17,7 @@ router.get('/', async function(req, res, next) {
 
   res.render('index', {
     title: snapit,
-    imageUrl : appRoot+'/tost.jpg',
+    imageUrl : '/images/tost.jpg',
   });
 });
 
