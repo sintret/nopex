e043d929c19dd9579d86e8638e93866d9bcbff43
andif_injani@bitbucket.com <andif@injani.com>
2019-03-04 15:43:18 +0700

    ok

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,33 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', async function(req, res, next) {
 
-
-  var imagePath = appRoot+"/public/images";
-
   const PiCamera = require('pi-camera');
   const myCamera = new PiCamera({
     mode: 'photo',
-    output: imagePath+'/tost.jpg',
+    output: '/home/nopex/public/images/test.jpg',
     width: 640,
     height: 480,
     nopreview: true,
   });
   var snapit  = await myCamera.snap();
 
+  myCamera.snap()
+      .then((result) => {
+        // Your picture was captured
+
+
+        console.log(result)
+
+      })
+      .catch((error) => {
+        // Handle your error
+      });
+
   res.render('index', {
     title: snapit,
-    imageUrl : '/images/tost.jpg',
+    imageUrl : '/images/test.jpg',
   });
+
 });
 
 module.exports = router;
